5076a9583518a899d4597d30c6fcfc833f714f08
Nawar Almuohi <almuohi.nawar@gmail.com>
2026-02-24 17:28:41 -0600

    Sprint 1 database schema, core queries and screenshots

---
+++ sprint1/database-core-queries.sql
@@ -0,0 +1,10 @@
+USE yaphub;
+
+-- Query 1: Shows all the users
+SELECT * FROM users;
+
+-- Query 2: Shows all follows relationships
+SELECT * FROM follows;
+
+-- Query 3: Shows all posts
+SELECT * FROM posts;
