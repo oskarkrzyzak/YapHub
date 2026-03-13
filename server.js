347fb4aa9978bb1be5572f1ffdc9df2ffb89426e
Oskar Krzyzak <oskar.krzyzak.ok@gmail.com>
2026-03-13 18:52:15 -0500

    Update database password in server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/images', express.static('images'));
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'marwip777',
+  password: 'password',
   database: 'yaphub'
 });
 
